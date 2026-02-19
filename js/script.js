cc3f48f131e758686ccce3d9321a35ffaaba1468
GuanKiid <87319056+GuanKiid@users.noreply.github.com>
2026-02-19 08:45:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,3 +28,22 @@ function openModal(product) {
 function closeModal() {
   document.getElementById("modal").style.display = "none";
 }
+
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  counter.innerText = '0';
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+    const increment = target / 100;
+    if(c < target){
+      counter.innerText = Math.ceil(c + increment);
+      setTimeout(updateCounter, 20);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
+});
